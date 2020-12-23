a4eb7066bbc5fecc51766bff47a11c9649662c8c
sinamna <sina3050@gmail.com>
2020-12-23 10:50:23 +0330

    added user schema

---
+++ models/User.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema= new mongoose.Schema({
+    studentNumber:{
+        type:String,
+        minlength:7,
+        maxlength:8,
+        unique:true,
+        sparse:true,
+        required:true,
+        validate(value){
+            if(!validator.isNumeric(value)){
+                throw new Error ("شماره دانشجویی نامعتبر است")
+            }
+        }
+        
+    },
+    password:{
+        type: String,
+        minlength:8,
+        required: true,
+        validate(value){
+            if(validator.isNumeric(value) || validator.isAlpha){
+                throw new Error("رمز عبور ضعیف می باشد")
+            }
+        }
+    }, 
+    
+    testCases:[
+        {
+            forQuestion:
+            {
+                type:mongoose.Types.ObjectId,
+                ref:"Question",
+                required:true
+            },
+            input:{
+                type: String,
+                require:true,
+            },
+            correctOutput:{
+                type:String
+            }
+        }
+    ],
+    
+    codes:[
+        {
+            forQuestion:{
+                type: mongoose.Types.ObjectId,
+                ref:"Question",
+                required: true
+            },
+            code:{
+                type: String,
+                require:true
+            }
+        }
+    ]
+});
+
+//TODO : add toJSON method
+
+//TODO : add jwt authentication
+
+
+userSchema.pre('save',async function(next){
+    const user=this;
+
+    if(!user.isModified("password")){
+        user.password=await bcrypt.hash(user.password,10);
+    }
+    next();
+});
+
+module.exports = new mongoose.model("User", userSchema);
+
+
