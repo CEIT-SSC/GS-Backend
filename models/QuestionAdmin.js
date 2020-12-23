1fd7043dce6ffded1f516d8d92c9bff6a0214507
sinamna <sina3050@gmail.com>
2020-12-23 11:14:20 +0330

    done the base of question admin

---
+++ models/QuestionAdmin.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt =require("jsonwebtoken");
+const { default: validator } = require("validator");
+
+
+const questionAdminSchema= new mongoose.Schema({
+    username:{
+        type: String,
+        unique:true,
+        required: true
+    },
+    password:{
+        type: String,
+        required:true,
+        validate(value){
+            if(validator.isNumeric(value)||validator.isAlpha){
+                throw new Error("رمز عبور ضعیف می باشد")
+            }
+        }
+    }
+});
+
+// TODO toJSON
+
+// TODO pre-save hash
+module.exports=new mongoose.model("QuestionAdmin", questionAdminSchema);
