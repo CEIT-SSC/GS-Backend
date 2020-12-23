38034985db3b58da0b48409cd16b9b10c7e7fdd2
sinamna <sina3050@gmail.com>
2020-12-23 11:41:35 +0330

    updated config file

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,10 @@ require("dotenv").config()
 
 const PORT=process.env.PORT
 const MONGODB_URL=process.env.MONGODB_URL
+const JWT_SECRET=process.env.JWT_SECRET
 
 module.exports={
     MONGODB_URL,
-    PORT
+    PORT,
+    JWT_SECRET
 }
