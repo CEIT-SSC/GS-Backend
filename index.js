3802b6522f86519a2db3fa0ceebbfa82fe43329a
sinamna <sina3050@gmail.com>
2020-12-24 00:18:25 +0330

    fixed port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const logger = require("./utils/logger");
 //connect to db
 require("./db/configureDB");
 
-app.listen(PORT, ()=>{
+app.listen(config.PORT, ()=>{
     logger.info(`Server is running on port ${config.PORT}`);
 });
