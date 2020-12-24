db3a796069dbc18a662c88472bb26a98651fd305
sinamna <sina3050@gmail.com>
2020-12-24 11:14:38 +0330

    fixed verifing bug

--- middlewares/superUserAuth.js
+++ middlewares/superUserAuth.js
@@ -4,11 +4,11 @@ const config = require("../utils/config")
 
 async function authenticateSuperUser(req,res,next){
     try{
-        const token=req.header('Authorization').replace('Bearer','');
+        const token=req.header('Authorization').replace('Bearer ','');
         const decoded = jwt.verify(token,config.JWT_SECRET);
         const superUser= await SuperUser.findOne({_id:decoded._id, 'tokens.token':token});
 
-        if(!admin)throw new Error("Please authenticate as admin");
+        if(!superUser)throw new Error("Please authenticate as admin");
         req.token=token;
         req.admin=superUser;
         next()
