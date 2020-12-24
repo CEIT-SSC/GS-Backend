8e865456ea515efb63453de3d43308fc4bfd2647
sinamna <sina3050@gmail.com>
2020-12-24 21:52:18 +0330

    done question admin auth

---
+++ middlewares/questionAdminAuth.js
@@ -0,0 +1,20 @@
+const QuestionAdmin = require("../models/QuestionAdmin");
+const SuperUser = require("../models/SuperUser");
+async function authenticateAdmin( req, res, next){
+    try{
+        const token=req.header('Authorization').replace('Bearer ','');
+        const decoded = jwt.verify(token,config.JWT_SECRET);
+        const superUser= await SuperUser.findOne({_id:decoded._id, 'tokens.token':token});
+        const questionAdmin = await QuestionAdmin.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if(!superUser && !questionAdmin )throw new Error("You don't have the premission");
+        req.token=token;
+        req.admin=superUser?superUser:questionAdmin;
+        next()
+    }catch(err){
+        res.status(401).send({
+            error:err.message
+        })
+    }
+};
+module.exports = authenticateAdmin;
