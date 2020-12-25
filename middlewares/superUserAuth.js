92f336f5223d292409331220e78c15e647963ed1
sinamna <sina3050@gmail.com>
2020-12-25 13:44:38 +0330

    small change in error text

--- middlewares/superUserAuth.js
+++ middlewares/superUserAuth.js
@@ -8,7 +8,7 @@ async function authenticateSuperUser(req,res,next){
         const decoded = jwt.verify(token,config.JWT_SECRET);
         const superUser= await SuperUser.findOne({_id:decoded._id, 'tokens.token':token});
         
-        if(!superUser)throw new Error("Please authenticate as admin");
+        if(!superUser)throw new Error("Please authenticate as super user");
         req.token=token;
         req.admin=superUser;
         console.log("super enable")
