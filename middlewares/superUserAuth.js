bade5b52acced8a9cbaafdd6764d1af1d370b53f
sinamna <sina3050@gmail.com>
2020-12-26 22:15:38 +0330

    super user auth refactored

--- middlewares/superUserAuth.js
+++ middlewares/superUserAuth.js
@@ -1,22 +1,28 @@
 const SuperUser = require("../models/SuperUser")
 const jwt = require ("jsonwebtoken");
 const config = require("../utils/config")
-
-async function authenticateSuperUser(req,res,next){
+async function authCheckSuperUser(req){
     try{
         const token=req.header('Authorization').replace('Bearer ','');
         const decoded = jwt.verify(token,config.JWT_SECRET);
         const superUser= await SuperUser.findOne({_id:decoded._id, 'tokens.token':token});
         
-        if(!superUser)throw new Error("Please authenticate as super user");
+        if(!superUser)return false;
         req.token=token;
         req.admin=superUser;
         console.log("super enable")
-        next()
+        return true;
     }catch(err){
+        return false
+    }
+}
+async function authenticateSuperUser(req,res,next){
+    if(await authCheckSuperUser(req)){
+        next();
+    }else{
         res.status(401).send({
-            error:err.message
-        })
+            error:'Please authenticate as admin'
+        });
     }
 }
 module.exports=authenticateSuperUser;
