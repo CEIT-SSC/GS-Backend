15ae536445fe4a4438a658ee4dc293ac0928559b
sinamna <sina3050@gmail.com>
2020-12-26 12:20:27 +0330

    added swagger hot docs

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}));
 
+
+// const swaggerJSDoc= require('swagger-jsdoc');
+const yaml =require("yamljs");
+const swaggerSetUp=yaml.load("./swagger.yaml");
+const swaggerUI= require('swagger-ui-express');
+
 //route handlers
 app.use(`/superarea`,require("./routers/superuser"));
 app.use('/questionadmin',require("./routers/questionAdmin"));
 app.use('/user',require("./routers/user"));
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerSetUp));
 
 app.use(unknownEndpointHandler);
 app.use(errorHandler);
