888f2cffd31a637b24d9b927938a2504a0437f32
sinamna <sina3050@gmail.com>
2020-12-28 00:06:34 +0330

    still in development

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,26 +1,39 @@
-const mutler = require ("mutler");
+const multer = require ("multer");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const logger = require("../utils/logger");
 
-const testCaseStorage = mutler.diskStorage({
+const testCaseStorage = multer.diskStorage({
     destination: function( req, file, cb){
-        cb(null,'./data/questions');
+        console.log(file)
+        cb(null,`./data/questions/${req.objectId}`);
     },
     filename: function( req, file, cb) {
-    
-    }
-});
-const uploadTestCase = multer ({
-    storage:testCaseStorage,
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(txt)$/)){
-            cb(new Error("please upload a text file"));
+        if(file.fieldname === "testGenerator"){
+            cb(null, `testGenerator_${req.objectId}`);
         }
-        cb(undefined,true);
+        else if (file.fieldname === "answer"){
+            cb(null, `answer_${req.objectId}`);
+        }
     }
 });
 
+const generateFilter = (req, file, cb)=>{
+    // if(file.fieldname ==="input" || file.fieldname==="output"){
+    //     if(!file.originalname.match(/\.(txt)$/)){
+    //         cb(new Error("please upload a text file"));
+    //     }
+    //     cb(undefined,true);
+    
+    if(file.fieldname === "testGenerator"){
+        cb(null,true);
+    }
+
+}
+const uploadTestCase = multer ({
+    storage:testCaseStorage,
+    fileFilter: generateFilter
+});
 const generateId = function (req,res,next){
     const id = mongoose.Types.ObjectId();
     req.objectId=id;
@@ -32,10 +45,13 @@ const generateDir= function (req,res,next){
             logger.error(err.message);
         }else{
             logger.info("directory created successfully");
+            next();
         }
     })
 }
 
 module.exports={
-    uploadTestCase
+    uploadTestCase,
+    generateId,
+    generateDir
 };
