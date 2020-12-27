5281289e0752fa5be40146588b425f4ae9f7ff86
sinamna <sina3050@gmail.com>
2020-12-28 00:06:17 +0330

    BIG BUG AND NOT TESTED

--- routers/question.js
+++ routers/question.js
@@ -1,12 +1,42 @@
 const router = require ("express").Router();
 const Question = require ("../models/Question");
 const {authenticateAdmin } = require("../middlewares/questionAdminAuth");
-const { uploadTestCase } = require ("../middlewares/upload")
-
+const { 
+    uploadTestCase,
+    generateId,
+    generateDir } = require ("../middlewares/upload")
+const uploadFields=[
+    {name:'answercase'},
+    {name:'testGenerator'}
+]
 //create question
-router.post("/", authenticateAdmin , uploadTestCase , async (req,res)=>{
-    
-})
+router.post("/", authenticateAdmin , generateId ,generateDir ,uploadTestCase.fields(uploadFields) , async (req,res)=>{
+    try{
+        console.log(req.files)
+        if(!req.body.name || !req.body.body){
+            throw new Error("please complete all fields");
+        }   
+        console.log(JSON.stringify(req.body))
+        const question= new Question({
+            _id:req.objectId,
+            forDate:new Date(),
+            name: req.body.name,
+            body: req.body.body,
+            examples: req.body.examples,
+            testGeneratorPath: req.files.testGenerator.path,
+            answerPath: req.files.answer[0].path
+        })
+
+        await question.save().then(()=>{
+            logger.info("question saved successfully");
+        })
+
+        res.status(201).send({question});
+    }catch(err){
+        res.status(400).send({error:err.message});
+    }
+
+});
 //get questions
 
 //get specific question
