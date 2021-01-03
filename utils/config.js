b5114079760a769057889d57b3f6eed2af6c7e39
sinamna <sina3050@gmail.com>
2021-01-03 23:36:19 +0330

    added base of test case generator script

--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,7 @@ const PORT=process.env.PORT
 const MONGODB_URL=process.env.MONGODB_URL
 const JWT_SECRET=process.env.JWT_SECRET
 const baseURL=process.env.baseURL
+const SCRIPT_PATH= process.env.SCRIPT_PATH
 module.exports={
     MONGODB_URL,
     PORT,
---
+++ utils/utils.js
@@ -0,0 +1,27 @@
+const { spawn } = require ("child_process");
+const { stderr } = require("process");
+const config = require ("./config");
+const logger = require ('./logger')
+
+
+function runScript(testGeneratorPath, studentNumber){
+    const script= spawn(`sh ${config.SCRIPT_PATH}`,[testGeneratorPath,studentNumber]);
+    script.on('error',error=>{
+        logger.error(error);
+        return 0;
+    });
+    script.stderr.on('data', data =>{
+        logger.error(`stderr:${data}`);
+        return 0;
+    });
+    script.stdout.on('data' , data=>{
+        logger.info('test cases generated successfully');
+        return data;
+    });
+    script.on('close' , code =>{
+        if(code == 0){
+            logger.error("couldn't create test cases for user");
+            return 0;
+        }
+    });
+}
