15c8b8c893039ea14db85c96f94c94502c7fa090
sinamna <sina3050@gmail.com>
2021-01-05 01:55:10 +0330

    add func to read from buffer

--- utils/utils.js
+++ utils/utils.js
@@ -11,37 +11,14 @@ async function runScript(scriptPath, studentNumber){
     }else{
         logger.error("couldn't create test cases for user");
     }
-    // script.stdout.on("data" , data=>{
-    //     logger.info('test cases generated successfully');
-    //     console.log(data.toString());
-    //     return data;
-    // });
-
-    // script.stderr.on('data', data =>{
-    //     logger.error(`stderr:${data}`);
-    //     return 0;
-    // });
-
-    // script.on('error',error=>{
-    //     logger.error(error);
-    //     return 0;
-    // });
-
-
-    // script.on('close' , code =>{
-    //     if(code != 0){
-    //         logger.error("couldn't create test cases for user");
-    //         return 0;
-    //     }
-    // });
 }
 
-async function saveTestCase(questionId,data){
-    await fs.writeFileSync(`./data/questions/${questionId}/saveTestCase.txt`,data);
+function readOutput(file){
+    return Buffer.from(file.buffer).toString('utf-8');
 }
 
 
 module.exports = {
     runScript,
-    saveTestCase
+    readOutput
 }
