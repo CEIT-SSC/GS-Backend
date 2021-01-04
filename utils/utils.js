647d0fd345256c94e656591ca0985ddaa1dc676c
sinamna <sina3050@gmail.com>
2021-01-04 11:12:04 +0330

    used synced spawn

--- utils/utils.js
+++ utils/utils.js
@@ -3,9 +3,9 @@ const { stderr } = require("process");
 const config = require ("./config");
 const logger = require ('./logger');
 const fs = require ("fs");
-async function runScript(testGeneratorPath, studentNumber){
+async function runScript(scriptPath, studentNumber){
 
-    const script= await spawnSync(`sh`,['./testcase.sh',testGeneratorPath,studentNumber]);
+    const script= await spawnSync(`sh`,['./testcase.sh',scriptPath,studentNumber]);
     if(!script.status){
         return script.stdout.toString();
     }else{
