c44e452b54b43e45f56aef4a6c4b06d295cb7714
sinamna <sina3050@gmail.com>
2021-01-04 09:57:02 +0330

    fixed opening script bug

--- utils/utils.js
+++ utils/utils.js
@@ -1,29 +1,40 @@
-const { spawn } = require ("child_process");
+const { spawnSync } = require ("child_process");
 const { stderr } = require("process");
 const config = require ("./config");
 const logger = require ('./logger')
+const util = require ('util');
+execFile = util.promisify(require("child_process").execFile);
+async function runScript(testGeneratorPath, studentNumber){
+
+    const script= await spawnSync(`sh`,['./testcase.sh',testGeneratorPath,studentNumber]);
+    if(!script.status){
+        return script.stdout.toString();
+    }else{
+        logger.error("couldn't create test cases for user");
+    }
+    // script.stdout.on("data" , data=>{
+    //     logger.info('test cases generated successfully');
+    //     console.log(data.toString());
+    //     return data;
+    // });
+
+    // script.stderr.on('data', data =>{
+    //     logger.error(`stderr:${data}`);
+    //     return 0;
+    // });
+
+    // script.on('error',error=>{
+    //     logger.error(error);
+    //     return 0;
+    // });
 
 
-function runScript(testGeneratorPath, studentNumber){
-    const script= spawn(`sh ${config.SCRIPT_PATH}`,[testGeneratorPath,studentNumber]);
-    script.on('error',error=>{
-        logger.error(error);
-        return 0;
-    });
-    script.stderr.on('data', data =>{
-        logger.error(`stderr:${data}`);
-        return 0;
-    });
-    script.stdout.on('data' , data=>{
-        logger.info('test cases generated successfully');
-        return data;
-    });
-    script.on('close' , code =>{
-        if(code == 0){
-            logger.error("couldn't create test cases for user");
-            return 0;
-        }
-    });
+    // script.on('close' , code =>{
+    //     if(code != 0){
+    //         logger.error("couldn't create test cases for user");
+    //         return 0;
+    //     }
+    // });
 }
 
 
