10898a3dab7051dc98b4b9a00f51fde7fbb6ff9e
sinamna <sina3050@gmail.com>
2021-01-07 01:06:59 +0330

    added get scoreboard , not tested

---
+++ routers/scoreboard.js
@@ -0,0 +1,43 @@
+
+const router = require ("express").Router();
+const User = require ("../models/User");
+
+router.get("/",async(req,res)=>{
+    try{
+        const allUsers = await User.find();
+        let allUserData = allUsers.map(user=>{
+            let penalty=0;
+            let userScore=0;
+            for(let code of user.codes){
+                code.populate('forQuestion',{forDate:1,score:1}).then(code=>{
+                    const forDate= code.forQuestion.forDate;
+                    const submittedDate = code.date;
+                    penalty = submittedDate-forDate;
+                    userScore+=code.forQuestion.score;
+                });
+                
+            }
+            return {
+                studentNumber: user.studentNumber,
+                penalty: penalty,
+                score: userScore
+            }
+        });
+        allUserData = allUserData.sort((fisrtUser,secondUser)=>{
+            if(fisrtUser.score > secondUser.score) return -1;
+            else if(fisrtUser.score == secondUser.score){
+                return fisrtUser.penalty<=secondUser.penalty?-1:1;
+            }else return 1;
+        });
+        // console.log(allUsers);
+        res.send(allUserData)
+    }catch(error){
+        res.status(500).send({
+            message:error.message
+        });
+    }
+});
+
+
+
+module.exports = router;
