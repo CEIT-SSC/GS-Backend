5b1eb98221f6504b8bc6b236929968363ab6e716
sinamna <sina3050@gmail.com>
2021-01-20 00:30:41 +0330

    setting a default port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require("./utils/logger");
 //connect to db
 require("./db/configureDB");
 
-app.listen(config.PORT, ()=>{
+app.listen(config.PORT ||3000 , ()=>{
     logger.info(`Server is running on port ${config.PORT}`);
 });
 module.exports= app;
