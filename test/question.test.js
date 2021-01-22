1dbaf7afd9f998d96b8933b43e8db4702115ff93
sinamna <sina3050@gmail.com>
2021-01-22 20:56:07 +0330

    tested post routes

--- test/question.test.js
+++ test/question.test.js
@@ -1,6 +1,6 @@
 const Question= require ("../models/Question");
 const QuestionAdmin = require ("../models/QuestionAdmin");
-
+const fs= require("fs");
 const chai =require ('chai');
 const chaiHttp = require ('chai-http');
 const app = require('../index');
@@ -10,27 +10,28 @@ chai.use(chaiHttp);
 describe("Question Test" , ()=>{
 
     let authToken;
-    before('creating questioadmin',(done)=>{
+    before('creating question admin',(done)=>{
+        //creating dummy question admin
         const questionAdmin = new QuestionAdmin({
             username: "dumbAdmin",
-            password: "dumbPass"
+            password: "dumbPass1"
         });
-        questionAdmin.save()
-            .then(()=>{
-                chai.request(app)
-                    .post("/questionadmin/login")
-                    .send({
-                        username: "dumbAdmin",
-                        password: "dumbPass"
-                    })
-                    .end((err,req)=>{
-                        if(err)done(err)
-                        req.should.have.status(200);
-                        req.body.should.have.property("token").not.empty;
-                        authToken=req.body.token;
-                        done();
-                    })
+        questionAdmin.save().then(()=>{
+            chai.request(app)
+            .post("/questionadmin/login")
+            .send({
+                username: "dumbAdmin",
+                password: "dumbPass1"
+            })
+            .end((err,res)=>{
+                if(err)done(err)
+                res.should.have.status(200);
+                res.body.should.have.property("token").not.empty;
+                authToken=res.body.token;
+                done();
             });
+        });
+        
     });
     after('droping dummy superusers',function(done){
         QuestionAdmin.findOneAndRemove({username: 'dumbAdmin'}).then(result=>{
@@ -38,5 +39,26 @@ describe("Question Test" , ()=>{
         }).catch(err=>done(err));
     });
 
-    
+    it("Creating new Qustion /question POST",(done)=>{
+        chai.request(app)
+            .post("/question/")
+            .set('Authorization',`Bearer ${authToken}`)
+            .set('content-type', 'multipart/form-data')
+            .field("date",Date.now())
+            .field("name","dummyName")
+            .field("body","dummy body baby")
+            .field("examples",'[{"input":"dummyIn","output":"dummyOut"}]')
+            .field("score",50)
+            .attach("testGenerator",fs.readFileSync("./test/dummyFiles/testGenerator.cpp"),'./test/testG,cpp')
+            .attach("answer",fs.readFileSync("./test/dummyFiles/answer.cpp"),'./test/asnwer.cpp')
+            .end((err,res)=>{
+                if(err)done(err)
+                res.should.have.status(201);
+                res.body.should.be.a("object");
+                res.body.should.have.property("name").equal("dummyName");
+                res.body.should.have.property("body").equal("dummy body baby");
+                res.body.should.have.property("score").equal(50);
+                done();
+            });
+    });
 });
