f9d2a6c01c618ce12222b743b9a2abf0b8c14fd1
sinamna <sina3050@gmail.com>
2021-02-03 02:55:25 +0330

    updated utils

--- utils/utils.js
+++ utils/utils.js
@@ -7,19 +7,20 @@ async function getTestCase(scriptPath, studentNumber){
 
     const script= await spawnSync(`sh`,['./scripts/testcase.sh',scriptPath,studentNumber]);
     if(!script.status){
-        return script.stdout.toString();
+        return script.stdout.toString().trim();
     }else{
-        logger.error("couldn't create test cases for user");
+        throw Error("couldn't create test cases for user");
     }
 }
 
 async function getAnswer(scriptPath, generatedTestCase){
-
-    const script= await spawnSync(`sh`,['./scripts/correctOutput.sh',scriptPath],{input : generatedTestCase});
+    const script= await spawnSync(`/bin/bash`,['./scripts/correctOutput.sh',scriptPath],{input: generatedTestCase});
     if(!script.status){
+        console.log(script.stdout.toString())
         return script.stdout.toString();
     }else{
-        logger.error("can't get the answer");
+        console.log(script.stderr.toString().trim())
+        throw Error("can't get the answer");
     }
 }
 
