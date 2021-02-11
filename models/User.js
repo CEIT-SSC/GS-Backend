fef409326b1ae23fe9b3d504bf8ac44e148c10a1
sinamna <sina3050@gmail.com>
2021-02-11 13:46:34 +0330

    updated error

--- models/User.js
+++ models/User.js
@@ -101,12 +101,12 @@ userSchema.statics.findByCredentials = async (studentNumber, password)=>{
     const user= await User.findOne({studentNumber});
 
     if(!user){
-        throw new Error("failed to identify user");
+        throw new Error("failed to identify user "+studentNumber);
     }
     const isPassMatch = await bcrypt.compare(password,user.password);
     // console.log(isPassMatch)
     if(!isPassMatch){
-        throw new Error("failded to identify user");
+        throw new Error("failded to identify user "+studentNumber);
     }
     return user;
 }
