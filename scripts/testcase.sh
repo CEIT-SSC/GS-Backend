d3ab742b465df0aedf8b646bce1accb5ac2e44fd
sinamna <sina3050@gmail.com>
2021-02-14 20:12:00 +0330

    updated script

--- scripts/testcase.sh
+++ scripts/testcase.sh
@@ -4,16 +4,7 @@ if [ $# -ne 2 ]; then
     echo "Invalid argument; usage: $0 {problem_id} {user_id}"
     exit 1
 fi
-SOLUTION_ADDRESS=$1
+QUESTION_PATH=$1
 USER_ID=$2
 
-file=$(ls $SOLUTION_ADDRESS)
-FILEDIR=${SOLUTION_ADDRESS%/*}
-base=${SOLUTION_ADDRESS##*/}
-FILENAME=${base%.*}
-
-if [[ $file == *.py ]]; then
-    python3 $SOLUTION_ADDRESS $USER_ID
-elif [[ $file == *.sh ]]; then
-    /bin/bash -c "$SOLUTION_ADDRESS $USER_ID"
-fi
+python3 $QUESTION_PATH $USER_ID
